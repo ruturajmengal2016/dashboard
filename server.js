71c7bc8432cb889ea96226e48500a50b1b60e37e
Ruturaj <ruturajmengal2016@gmail.com>
2023-04-09 15:43:33 +0530

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header({
     "Access-Control-Allow-Origin": "https://nobelium-dashboard.onrender.com",
-    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Origin": "*",
   });
   next();
 });
