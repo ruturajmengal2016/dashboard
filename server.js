790e6eb1558374b01c840b50a427e3295972abc3
Ruturaj <ruturajmengal2016@gmail.com>
2023-04-10 00:15:15 +0530

    commit

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post("/post", async (req, res) => {
     data.data.push(req.body);
     fs.writeFileSync("./Database/data.json", JSON.stringify(data, null, 2));
     res.status(201);
-    res.send(`send successfully...${req.body}`);
+    res.send(`send successfully...${data}`);
   } catch (error) {
     throw new Error(error);
   }
